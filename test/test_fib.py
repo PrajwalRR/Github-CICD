dd95e6c9afeb963ff51de325087a014466173836
PrajwalRR <prajwalrrnayak@gmail.com>
2025-08-20 10:19:41 +0530

    Fix import issue by adding __init__.py and updating PYTHONPATH

--- test/test_fib.py
+++ test/test_fib.py
@@ -1,5 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+from fib import fibonacci
 import pytest
-from src.fib import fibonacci
 
 def test_fibonacci_zero():
     assert fibonacci(0) == 0
